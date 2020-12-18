90506be12a44101bdea81d633fdfbe54bfa695aa
Natan Nascimento <natanascimentom@outlook.com>
2020-12-18 01:38:25 -0300

    Adicionando método de leitura para arquivos XML

--- devnet-associate-prep/Software Developement and Design/processing_data.py
+++ devnet-associate-prep/Software Developement and Design/processing_data.py
@@ -4,11 +4,11 @@ from os import listdir
 from os.path import isfile, join
 #External Library
 import yaml
+import xmltodict
 
 class processing_data:
     def __init__(self,path):
         self.path = path
-        #self.filepath = self.getFiles()
     
     def getFiles(self):
         files = [f for f in listdir(self.path) if isfile(join(self.path, f))]
@@ -16,25 +16,35 @@ class processing_data:
             if file.endswith(".json"):
                 filepath_json = os.path.join(self.path, file)
                 print(filepath_json)
-                self.getJson(filepath_json)
+                self.getJsonData(filepath_json)
             elif file.endswith(".yaml"):
                 filepath_yaml = os.path.join(self.path, file)
                 print(filepath_yaml)
-                self.getYaml(filepath_yaml)
+                self.getYamlData(filepath_yaml)
+            elif file.endswith(".xml"):
+                filepath_xml = os.path.join(self.path, file)
+                print(filepath_xml)
+                self.getXMLData(filepath_xml)
 
             
-    def getJson(self,filepath):
+    def getJsonData(self,filepath):
         #Open the specified database file for reading and perform loading
         with open(filepath, 'r') as handle:
             data_json = json.load(handle)
             print(data_json)
     
-    def getYaml(self, filepath):
+    def getYamlData(self, filepath):
         #Open the specified database file for reading and perform loading
         with open(filepath, 'r') as handle:
             data_yaml = yaml.safe_load(handle)
             print(data_yaml)
-
+            
+    def getXMLData(self, filepath):
+        #Open the specified database file for reading and perform loading
+        with open(filepath, 'r') as handle:
+            data_xml = xmltodict.parse(handle.read())["root"]
+            print(data_xml)
+            
 if __name__ == "__main__":
     mypath = "data"
     main = processing_data(mypath)
