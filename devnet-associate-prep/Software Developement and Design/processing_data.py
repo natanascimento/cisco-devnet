dee50814e17f86590c0c3c71974da1ec0ed7f7aa
Natan Nascimento <natanascimentom@outlook.com>
2020-12-18 01:35:31 -0300

    Alterando estrutura do processing_data e alterando estrutura do data/data.yaml

--- devnet-associate-prep/Software Developement and Design/processing_data.py
+++ devnet-associate-prep/Software Developement and Design/processing_data.py
@@ -2,28 +2,42 @@ import json
 import os
 from os import listdir
 from os.path import isfile, join
+#External Library
+import yaml
 
 class processing_data:
     def __init__(self,path):
         self.path = path
-        self.filepath = self.getFiles()
+        #self.filepath = self.getFiles()
     
     def getFiles(self):
         files = [f for f in listdir(self.path) if isfile(join(self.path, f))]
         for file in files:
-            self.filepath = os.path.join(self.path, file)
-            return self.filepath
+            if file.endswith(".json"):
+                filepath_json = os.path.join(self.path, file)
+                print(filepath_json)
+                self.getJson(filepath_json)
+            elif file.endswith(".yaml"):
+                filepath_yaml = os.path.join(self.path, file)
+                print(filepath_yaml)
+                self.getYaml(filepath_yaml)
+
             
-    def getJson(self):
-        if self.filepath.endswith(".json"):
-            #Open the specified database file for reading and perform loading
-            with open(self.filepath, 'r') as handler:
-                self.data = json.load(handler)
-                print(self.data)
+    def getJson(self,filepath):
+        #Open the specified database file for reading and perform loading
+        with open(filepath, 'r') as handle:
+            data_json = json.load(handle)
+            print(data_json)
+    
+    def getYaml(self, filepath):
+        #Open the specified database file for reading and perform loading
+        with open(filepath, 'r') as handle:
+            data_yaml = yaml.safe_load(handle)
+            print(data_yaml)
 
 if __name__ == "__main__":
-    mypath = "devnet-associate-prep\\Software Developement and Design\\data"
+    mypath = "data"
     main = processing_data(mypath)
-    main.getJson()
+    main.getFiles()
 
 
