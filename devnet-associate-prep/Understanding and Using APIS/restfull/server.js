fa2ddce9d232b364c0dde9143b76a896cce21691
Natan Nascimento <natanascimentom@outlook.com>
2020-12-22 22:15:22 -0300

    Adicionando todos os verbos HTTP e alterando o .gitignore

--- devnet-associate-prep/Understanding and Using APIS/restfull/server.js
+++ devnet-associate-prep/Understanding and Using APIS/restfull/server.js
@@ -18,12 +18,31 @@ app.get("/clients/:id", function(req, res){
 });
 
 
-app.post("/clients", function(req, res){});
+app.post("/clients", function(req, res){
+    const { name, email } = req.body;
 
-app.put("/clients", function(req, res){});
-app.put("/clients/:id", function(req, res){});
-app.delete("/clients", function(req, res){});
-app.delete("/clients/:id", function(req, res){});
+    res.json({ name, email });
+});
+
+app.put("/clients/:id", function(req, res) {
+    const { id } = req.params;
+    const client = data.find(cli => cli.id == id);
+  
+    if (!client) return res.status(204).json();
+  
+    const { name } = req.body;
+  
+    client.name = name;
+  
+    res.json(client);
+  });
+
+app.delete("/clients/:id", function(req, res) {
+    const { id } = req.params;
+    const clientsFiltered = data.filter(client => client.id != id);
+  
+    res.json(clientsFiltered);
+  });
 
 app.listen(3000, function(){
     console.log('Server is running');
