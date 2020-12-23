e7c9860e0b3b362f5becb3df7a601551bd700e2d
Natan Nascimento <natanascimentom@outlook.com>
2020-12-22 22:16:00 -0300

    Alterando estrutura de requisições e definindo todos os gets

--- devnet-associate-prep/Understanding and Using APIS/request/main.py
+++ devnet-associate-prep/Understanding and Using APIS/request/main.py
@@ -1,11 +1,37 @@
 import requests
 import json
 
-uri = 'http://localhost:3000/clients/15'
+class Clients:
+    
+    def __init__(self):
+        self.uri = "http://localhost:3000/clients"
+        
+    def getClients(self):
+        req = requests.get(self.uri)
+        return req
 
-r = requests.get(uri)
+    def getClientsById(self, id):
+        uri = self.uri + '/' + id
+        req = requests.get(uri)
+        return req
+    
+    def menu(self):
+        option = int(input(' ---=MENU=--- \n 1 - Captar todos os Clientes\n 2 - Captar Cliente por ID\n ----====----\nOpção: '))
 
-if r.status_code == 200:
-    print(r.json())
-else:
-    print(r.status_code)
+        if option == 1:
+            res = self.getClients()
+            if res.status_code == 200:
+                print(res.json())
+            else:
+                print(res.status_code)
+        if option == 2:
+            id = input('\n ----====---- \n Informe o ID do Cliente que deseja consultar: ')
+            res = self.getClientsById(id)
+            if res.status_code == 200:
+                print(res.json())
+            else:
+                print(res.status_code)
+
+if __name__ == '__main__':
+    main = Clients()
+    main.menu()
